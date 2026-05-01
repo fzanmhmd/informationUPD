b8c33544387a205668b536b5dcaba84c16ec3701
Muhammad Fauzan <ojan14021@gmail.com>
2026-05-01 19:40:08 +0700

    Update login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,10 +1,23 @@
-document.getElementById('login').addEventListener('click', function(){
-  const username = document.getElementById('username').value
-  const password = document.getElementById('password').value
+function masuk() {
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
 
-  if (username === 'galeh' && password === '123' || username === 'ojan' && password === '123' ){
-    window.location.href = '/pages/tampilan.html'
-  } else{
-    alert(`Username atau Password Anda Salah!`)
+  if (
+    (username === 'galeh' && password === '123') ||
+    (username === 'ojan' && password === '123')
+  ) {
+    ShowToast('Login Berhasil. Tunggu Sebentar...');
+  setTimeout(() => {
+    window.location.href = '/pages/tampilan.html';
+  }, 1500);
+  } else {
+    ShowToast('Username atau Password Anda Salah!');
   }
-});
+}
+
+function ShowToast(pesan){
+  const p = document.getElementById(`toast`)
+  p.innerText = pesan
+  p.classList.add(`show`)
+  setTimeout(() => p.classList.remove('show'), 3000)
+}
